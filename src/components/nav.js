966a893c28580ec68dfe6407ba589ef945f92f0a
jd2rogers2 <jd2rogers2@gmail.com>
2019-08-08 15:21:03 -0400

    slack link, code of conduct content

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,13 +4,20 @@ import { Link } from "gatsby"
 import React from "react"
 import "./nav.css"
 
+// slack linki found via google
+// https://boston-javascript-slackin.herokuapp.com/
+
+// slack link from old page that other said didn't work (works ok for james)
+// https://bostonjavascript.slack.com/join/shared_invite/enQtMjQyNzkwNDM4OTYyLTU3MTExMzI0YTYwZWU3M2UyMWJjNGMxZGEwOTVlYWNhODM1NWE4MGY3MDkxMGE1YzIwMDc1NTFmMzJlMTEzNDI
+
 const Nav = () => {
+  const slackLink = "https://bostonjavascript.slack.com/join/shared_invite/enQtMjQyNzkwNDM4OTYyLTU3MTExMzI0YTYwZWU3M2UyMWJjNGMxZGEwOTVlYWNhODM1NWE4MGY3MDkxMGE1YzIwMDc1NTFmMzJlMTEzNDI";
   return (
       <div id="navWrapper">
         <div id="navComponent">
             <Link className="navLink" to="/">Past Events</Link>
             <Link className="navLink" to="/CodeOfConduct/">Code of Conduct</Link>
-            <Link className="navLink" to="/">Slack Community</Link>
+            <a className="navLink" href={slackLink} target="_blank">Slack Community</a>
             <Link className="navLink" to="/">Meetup.com</Link>
         </div>
     </div>
