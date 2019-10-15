db67b8b72928af7a419b07259a3ce0db45175f50
jd2rogers2 <jd2rogers2@gmail.com>
2019-10-15 16:24:33 -0400

    remove comments and stuff

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,13 @@
 import { Link } from "gatsby"
-// import Img from "gatsby-image"
-// import PropTypes from "prop-types"
 import React from "react"
 import "./nav.css"
 
-// slack linki found via google
-// https://boston-javascript-slackin.herokuapp.com/
-
-// slack link from old page that other said didn't work (works ok for james)
-// https://bostonjavascript.slack.com/join/shared_invite/enQtMjQyNzkwNDM4OTYyLTU3MTExMzI0YTYwZWU3M2UyMWJjNGMxZGEwOTVlYWNhODM1NWE4MGY3MDkxMGE1YzIwMDc1NTFmMzJlMTEzNDI
-
-//            <Link className="navLink" to="/MeetUp/">Meetup.com</Link>
-
 const Nav = () => {
-  const slackLink = "https://bostonjavascript.slack.com/join/shared_invite/enQtMjQyNzkwNDM4OTYyLTU3MTExMzI0YTYwZWU3M2UyMWJjNGMxZGEwOTVlYWNhODM1NWE4MGY3MDkxMGE1YzIwMDc1NTFmMzJlMTEzNDI";
+  const slackLink = "https://boston-javascript-slackin.herokuapp.com/";
   return (
       <div id="navWrapper">
         <div id="navComponent">
-            <Link className="navLink" to="/">Past Events</Link>
+            <Link className="navLink" to="/">Home</Link>
             <Link className="navLink" to="/CodeOfConduct/">Code of Conduct</Link>
             <a className="navLink" href={slackLink} target="_blank" rel="noopener noreferrer">Slack Community</a>
             <a className="navLink" href='https://www.meetup.com/boston_JS' target="_blank" rel="noopener noreferrer">Meetup.com</a>
@@ -25,13 +15,5 @@ const Nav = () => {
     </div>
   );
 }
-
-// Nav.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
-
-// Nav.defaultProps = {
-//   siteTitle: ``,
-// }
 
 export default Nav
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,16 @@
 import React from "react"
-// import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-// import { eventsInitialVal, useEvents } from '../hooks';
-// const { events, handleEventsChange } = useEvents(eventsInitialVal);
-
-// const MEETUP_BASE_URL = "https://api.meetup.com/boston_JS/events";
-
 class IndexPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      events: [],
-    }
-  }
-
   render() {
     return (
       <Layout>
         <SEO title="Boston JS" />
-        <h1>Upcoming Meetups!</h1>
-        {this.state.events.length ? (
-          <p>yay</p>
-        ) : (
-          <p>no events to show</p>
-        )}
-
+        <h1>home page</h1>
       </Layout>
     );
   }
 }
-
 
 export default IndexPage
